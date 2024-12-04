c8c22756b1d68af91030387abe728634019ae1a3
Javier Ortiz <javieroc@gmail.com>
2024-12-04 23:50:08 +0100

    chore using createBrowserRouter y lazy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
+
+// Componentes lazy
+const Home = () => <div><h1>Página Principal</h1><p>Bienvenido a mi aplicación de prueba</p></div>
+const About = () => <div><h1>Acerca de</h1><p>Esta es una página de prueba</p></div>
+
+// Componente Layout
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <main style={{ padding: '1rem' }}>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Layout />}
+      errorElement={<div>Error!</div>}
+    >
+      <Route
+        index
+        lazy={async () => {
+          return { Component: Home }
+        }}
+      />
+      <Route
+        path="/about"
+        lazy={async () => {
+          return { Component: About }
+        }}
+      />
+    </Route>
+  ),
+  {
+    basename: '/'
+  }
+)
+
+// Debug helper
+router.subscribe((state) => {
+  console.log('Navigation state:', state);
+});
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <main style={{ padding: '1rem' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
